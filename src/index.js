8864bc3a75b6ff9795b2ccf852e0f1723f72eb38
Lucas Bersier <contact@lucasbersier.com>
2019-08-26 22:06:09 -0400

    Не надо тратить ману на новые скины

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 const $html = document.documentElement;
+const modes = {
+  1: {
+    color: '#00adef',
+  },
+  2: {
+    color: '#ff9dcc',
+  },
+  3: {
+    color: '#ffff76',
+  },
+  4: {
+    color: '#000',
+  },
+};
 const player = {
   x: 0,
   y: 0,
   click: false,
+  mode: modes[0],
 };
 const board = {
   w: 420,
@@ -22,17 +37,33 @@ document.addEventListener('mousemove', (e) => {
   if (player.y > (board.h / 2)) player.y = board.h / 2;
   if (player.x < -(board.w / 2)) player.x = -board.w / 2;
   if (player.y < -(board.h / 2)) player.y = -board.h / 2;
-});
+}, false);
 
 document.addEventListener('mousedown', (e) => {
   player.click = true;
   $html.style.setProperty('--cursorScale', '0.5');
-});
+}, false);
 
 document.addEventListener('mouseup', (e) => {
   player.click = false;
   $html.style.setProperty('--cursorScale', '1');
-});
+}, false);
+
+document.addEventListener('keydown', (e) => {
+  if (modes[e.key]) {
+    player.mode = modes[e.key];
+    $html.style.setProperty('--theme', player.mode.color);
+  }
+}, false);
+
+document.addEventListener('pointerlockchange', (e) => {
+  if (document.pointerLockElement) {
+    $html.classList.add('ingame');
+  } else {
+    console.log('a');
+    $html.classList.remove('ingame');
+  }
+}, false);
 
 const draw = () => {
   $html.style.setProperty('--mouse-x', `${player.x}px`);
@@ -47,7 +78,6 @@ const init = () => {
 
 document.querySelector('#start').onclick = () => {
   document.body.requestPointerLock();
-  $html.classList.add('ingame');
   init();
   draw();
 };
--- watch.js
+++ watch.js
@@ -3,7 +3,6 @@ const { exec } = require('child_process');
 
 watch('./src', { recursive: true }, (evt, name) => {
   exec('yarn run build', (err, stdout, stderr) => {
-    if (err) console.log(err, name);
-    else { console.log('%s changed.', name); }
+    console.log(err, stdout, stderr);
   });
 });
