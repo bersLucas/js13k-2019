d7486e7bae53178eefeb501b017bf2efc11f54a4
Lucas Bersier <contact@lucasbersier.com>
2019-08-27 23:05:26 -0400

    Раскройся трайбл вайб нам снова, будет всем приятнее

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const $html = document.documentElement;
+const $container = document.querySelector('#container');
+
 const modes = {
   1: {
     color: '#00adef',
@@ -13,24 +15,37 @@ const modes = {
     color: '#000',
   },
 };
+
 const player = {
   x: 0,
   y: 0,
   click: false,
   mode: modes[0],
 };
+
 const board = {
   w: 420,
   h: 320,
 };
 
+const ripple = () => {
+  const elem = document.createElement('div');
+  elem.className = 'ripple';
+  elem.style.backgroundColor = player.mode.color;
+  elem.style.transform = `translate(${player.x}px, ${player.y}px)`;
+  setTimeout(() => {
+    elem.remove();
+  }, 500);
+  return elem;
+};
+
 document.addEventListener('mousemove', (e) => {
   if (player.click) {
-    player.x += e.movementX / 7;
-    player.y += e.movementY / 7;
+    player.x += e.movementX / 8;
+    player.y += e.movementY / 8;
   } else {
-    player.x += e.movementX;
-    player.y += e.movementY;
+    player.x += e.movementX / 1.5;
+    player.y += e.movementY / 1.5;
   }
 
   if (player.x > (board.w / 2)) player.x = board.w / 2;
@@ -39,12 +54,12 @@ document.addEventListener('mousemove', (e) => {
   if (player.y < -(board.h / 2)) player.y = -board.h / 2;
 }, false);
 
-document.addEventListener('mousedown', (e) => {
+document.addEventListener('mousedown', () => {
   player.click = true;
   $html.style.setProperty('--cursorScale', '0.5');
 }, false);
 
-document.addEventListener('mouseup', (e) => {
+document.addEventListener('mouseup', () => {
   player.click = false;
   $html.style.setProperty('--cursorScale', '1');
 }, false);
@@ -53,27 +68,43 @@ document.addEventListener('keydown', (e) => {
   if (modes[e.key]) {
     player.mode = modes[e.key];
     $html.style.setProperty('--theme', player.mode.color);
+    $container.appendChild(ripple());
   }
 }, false);
 
-document.addEventListener('pointerlockchange', (e) => {
+document.addEventListener('pointerlockchange', () => {
   if (document.pointerLockElement) {
     $html.classList.add('ingame');
   } else {
-    console.log('a');
-    $html.classList.remove('ingame');
+    // $html.classList.remove('ingame');
   }
 }, false);
 
-const draw = () => {
+let start = null;
+const draw = (timestamp) => {
   $html.style.setProperty('--mouse-x', `${player.x}px`);
   $html.style.setProperty('--mouse-y', `${player.y}px`);
+
+  const dt = timestamp - start;
+  start = timestamp;
+
+  enemies.forEach((enemy) => {
+    enemy.move(dt);
+  });
   requestAnimationFrame(draw);
 };
 
 const init = () => {
   $html.style.setProperty('--board-w', `${board.w}px`);
   $html.style.setProperty('--board-h', `${board.h}px`);
+
+  const enemySpawnInterval = setInterval(() => {
+    enemies.push(new Enemy(-200 + Math.ceil(Math.random() * 400), -200 + Math.ceil(Math.random() * 400), 5));
+  }, 250);
+
+  setTimeout(() => {
+    clearInterval(enemySpawnInterval);
+  }, 2000);
 };
 
 document.querySelector('#start').onclick = () => {
@@ -81,3 +112,25 @@ document.querySelector('#start').onclick = () => {
   init();
   draw();
 };
+
+// TODO: Import this
+const enemies = [];
+
+class Enemy {
+  constructor(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    const elem = document.createElement('div');
+    elem.className = 'enemy';
+    elem.style.transform = `translate(${this.x}px, ${this.y}px)`;
+    $container.appendChild(elem);
+    this.elem = elem;
+
+    this.rad = Math.atan2(this.y - player.y, this.x - player.x);
+    elem.innerHTML = this.rad;
+  }
+
+  move(dt) {
+
+  }
+}
--- watch.js
+++ watch.js
@@ -1,8 +1,9 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 const watch = require('node-watch');
 const { exec } = require('child_process');
 
 watch('./src', { recursive: true }, (evt, name) => {
-  exec('yarn run build', (err, stdout, stderr) => {
-    console.log(err, stdout, stderr);
+  exec('yarn run build', () => {
+    process.stdout(name);
   });
 });
